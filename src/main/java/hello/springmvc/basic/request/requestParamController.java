eef499aea0d884a8e97276b097ee61180894427b
HaeChan Kim <bluesun147@naver.com>
2022-08-18 21:56:00 +0900

    HTTP 요청 파라미터 - @ModelAttribute

--- src/main/java/hello/springmvc/basic/request/requestParamController.java
+++ src/main/java/hello/springmvc/basic/request/requestParamController.java
@@ -1,12 +1,17 @@
 package hello.springmvc.basic.request;
 
+import hello.springmvc.basic.HelloData;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Map;
 
 @Slf4j
 @Controller
@@ -18,5 +23,78 @@ public class requestParamController {
 
         log.info("username={}, age={}", username, age);
         response.getWriter().write("ok");
+    }
+
+    @ResponseBody // RestController처럼 뷰 조회 안하고 바로
+    @RequestMapping("/request-param-v2")
+    public String requestParamV2(
+            @RequestParam("username") String memberName,
+            @RequestParam("age") int memberAge) {
+
+        log.info("username={}, age={}", memberName, memberAge);
+        return "ok";
+    }
+
+    @ResponseBody // RestController처럼 뷰 조회 안하고 바로
+    @RequestMapping("/request-param-v3")
+    public String requestParamV3(
+            @RequestParam String username, // 변수명 파라미터 이름과 같으면 생략 가능
+            @RequestParam int age) {
+
+        log.info("username={}, age={}", username, age);
+        return "ok";
+    }
+
+    @ResponseBody // RestController처럼 뷰 조회 안하고 바로
+    @RequestMapping("/request-param-v4")
+    public String requestParamV4(String username, int age) { // @ 그냥 쓰는거 추천
+        log.info("username={}, age={}", username, age);
+        return "ok";
+    }
+
+    @ResponseBody // RestController처럼 뷰 조회 안하고 바로
+    @RequestMapping("/request-param-required")
+    public String requestParamRequired(
+            @RequestParam String username,
+            @RequestParam(required = true) int age) { // true가 디폴트. 안써도됨.
+        log.info("username={}, age={}", username, age);
+        return "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-default")
+    public String requestParamDefault(
+            @RequestParam(required = true, defaultValue = "guest") String username,
+            @RequestParam(required = false, defaultValue = "-1") int age) { // true가 디폴트. 안써도됨.
+        log.info("username={}, age={}", username, age);
+        return "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-map")
+    public String requestParamMap(@RequestParam Map<String, Object> paramMap) { // true가 디폴트. 안써도됨.
+        log.info("username={}, age={}", paramMap.get("username"), paramMap.get("age"));
+        return "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping("/model-attribute-v0")
+    public String modelAttributeV0(@RequestParam String username, @RequestParam int age) {
+        HelloData helloData = new HelloData();
+        helloData.setUsername(username);
+        helloData.setAge(age);
+
+        log.info("username={}, age={}", helloData.getUsername(), helloData.getAge());
+        log.info("helloData={}", helloData);
+        return "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping("/model-attribute-v1") // 자동으로
+    public String modelAttributeV1(@ModelAttribute HelloData helloData) {
+
+        log.info("username={}, age={}", helloData.getUsername(), helloData.getAge());
+        log.info("helloData={}", helloData);
+        return "ok";
     }
 }
